f71d6acc82f2f55e85a395f4ae3906abe1160291
Giridhar_Sai_Chikkala <giridharsaichikkala@gmail.com>
2025-04-04 21:29:46 +0530

    added AuthLayout file

---
+++ src/components/AuthLayout.jsx
@@ -0,0 +1,25 @@
+// this component is used ti prodect routes its like container where user if logged in only can enter the routes
+import React, {useEffect, useState} from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+export default function Protected({children, authentication = true}) {// filenameand function name can be different 
+
+    const navigate = useNavigate();
+    const authStatus = useSelector((state) => state.auth.status);
+    const [loding, setloading] = useState(true);
+
+    useEffect(() => { //understand it on your own 
+        //will understand better during the routing part
+        if(authentication && authentication !== authStatus){
+            navigate('/login')
+        }
+        else if(!authentication && authentication !== authStatus){
+            navigate('/')
+        }
+    }, [authStatus, navigate, authentication])
+    
+
+  return loding ? <p>Loading...</p> : {children}
+}
+
